c73e9aa4cba4119bb1d42a503c078332e658a7e2
Erik Nelson <erik@nsk.io>
2016-08-10 14:15:39 -0400

    Cleaned up dev_srv

--- dev_srv.js
+++ dev_srv.js
@@ -23,6 +23,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
+////////////////////////////////////////////////////////////
+// socket.io
+////////////////////////////////////////////////////////////
+io.on('connection', socket => {
+  console.log('user connected!');
+  socket.on('disconnect', () => console.log('user disconnected'));
+});
+////////////////////////////////////////////////////////////
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'templates', 'index.html'));
 });
@@ -30,24 +39,38 @@ app.get('/', (req, res) => {
 const tasks = {};
 app.post('/tasks', (req, res) => {
   console.log(req.body);
-  console.log('executing task name: ', req.body.task.name);
   executeNewTask(req.body.task).then(task => {
-    console.log('finished executing task!', task);
-    tasks[task.id] = task;
-    res.send({task})
-  });
-});
+    const newTaskId = task.id;
+    const tickToComplete = genTickToComplete();
+    const tickPeriod = genTickPeriod();
 
-////////////////////////////////////////////////////////////
-// socket.io
-////////////////////////////////////////////////////////////
-//let counter = 1;
-io.on('connection', socket => {
-  console.log('user connected!');
-  socket.on('disconnect', () => console.log('user disconnected'));
-  //const interval = setInterval(() => socket.emit('message', {
-    //foo: counter++
-  //}), 1000);
+    task.progress = 0;
+    newTask = {
+      task: task,
+      ticks: 0
+    };
+
+    res.send({task})
+
+    const tickf = () => {
+      const taskRecord = tasks[newTaskId];
+
+      if(taskRecord.ticks == tickToComplete) {
+        clearInterval(taskRecord.interval);
+        taskRecord.task.progress = 1;
+        io.emit(taskChannel(taskRecord.task), {task: taskRecord.task});
+        delete tasks[newTaskId].interval;
+        return;
+      }
+
+      const task = taskRecord.task;
+      task.progress = (taskRecord.ticks++ / tickToComplete).toPrecision(2);
+      io.emit(taskChannel(task), {task});
+    };
+
+    newTask['interval'] = setInterval(tickf, tickPeriod);
+    tasks[newTaskId] = newTask;
+  });
 });
 
 const port = process.env.PORT || 3000;
@@ -63,8 +86,24 @@ http.listen(port, 'localhost', err => {
 function executeNewTask(task) {
   return new Promise((res, rej) => {
     uuid((err, id) => {
-      console.log('uuid task id -> ', id);
       res(merge(task, {id}));
     });
   });
 }
+
+function genTickToComplete() {
+  return rand(7, 15);
+}
+
+function genTickPeriod() {
+  return rand(500, 2000);
+}
+
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function taskChannel(task) {
+  return `/task/${task.id}`;
+}
+
